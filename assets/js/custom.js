459612f6486a9b44f98168f37947ee3ac00a1359
Guilherme de Mello <guillemello@outlook.com>
2018-04-25 01:26:02 -0300

    Alterações nas media queries

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -5,11 +5,13 @@ $(window).scroll(function() {
     $("nav#menu").removeClass("transparent");
     $("nav#menu").addClass("nav-flat");
     $("nav#menu ul li a").addClass("navbar-scroll");
+    $("#logo").attr("src", "assets/images/idot-logo-roxa.png");
 
   } else {
     $("nav#menu").removeClass("nav-flat");
     $("nav#menu").addClass("transparent");
     $("nav#menu ul li a").removeClass("navbar-scroll");
+    $("#logo").attr("src", "assets/images/idot-logo-branca.png");
 
   }
 });
