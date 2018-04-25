ae7af4913d339f5072cd3b6cf29b6808a99d2f81
Murilo Medeiros <murilo_medeiros98@hotmail.com>
2018-04-24 23:14:33 -0300

    Criação do site institucional iDot

---
+++ assets/js/custom.js
@@ -0,0 +1,32 @@
+$(window).scroll(function() {
+  var scroll = $(window).scrollTop();
+
+  if (scroll >= 100) {
+    $("nav#menu").removeClass("transparent");
+    $("nav#menu").addClass("nav-flat");
+    $("nav#menu ul li a").addClass("navbar-scroll");
+
+  } else {
+    $("nav#menu").removeClass("nav-flat");
+    $("nav#menu").addClass("transparent");
+    $("nav#menu ul li a").removeClass("navbar-scroll");
+
+  }
+});
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+});
